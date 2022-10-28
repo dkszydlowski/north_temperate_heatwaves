3d2eed97f5cbf00a29e2386f1f221c4ebb60ecbf
Katie Bollini <katiebollini@Katies-MacBook-Air.local>
2022-10-28 17:02:00 -0500

    2013-2015

--- Step0_DataCleaning.R
+++ Step0_DataCleaning.R
@@ -309,15 +309,39 @@ summary(as.factor(dt1$Lake))
 detach(dt1)               
 
 
+##### Cleaning Cascade Sonde Data 2013-2015#####
 
 sonde13_15 = dt1
 
+sonde13_15=sonde13_15 %>%
+  rename(lake=Lake, year=Year) %>%
+  mutate(doyCat=trunc(DoY))
+
+
+sonde13_15mean=sonde13_15 %>%
+  group_by(lake, year, doyCat) %>%
+  summarize(mean_temp=mean(Temp_HYLB, na.remove = TRUE),
+            mean_chl=mean(Chl_HYLB, na.remove = TRUE),
+            mean_pH=mean(PH_HYLB),
+            mean_doSat=mean(DOsat_calc_HYLB),
+            mean_do=mean(DO_HYLB))
+
+sonde13_15mean=sonde13_15mean %>%
+  mutate(date=as.Date(doyCat, origin=paste(year-1, "-12-31", sep="")))
+
+sonde13_15_mean_L = sonde13_15mean %>%
+  filter(lake == "Paul", year == "2013")
+
+sonde13_15_mean_L$normChl = sonde13_15_mean_L$mean_chl/sonde13_15_mean_L$mean_chl[1]*100
+sonde13_15_mean_L$normTemp = sonde13_15_mean_L$mean_temp/sonde13_15_mean_L$mean_temp[1]*100
+
+sonde13_15_mean_L$normChl = na.approx(sonde13_15_mean_L$normChl)
 
 
 
 
-##### Testing calculating heatwaves #####
-hwTest <- sonde08_11mean %>% rename(t = date, temp = mean_temp) %>% filter(lake == "Paul") %>% select(t, temp)
+##### 2008-2011 calculating heatwaves #####
+hwTest1 <- sonde08_11mean %>% rename(t = date, temp = mean_temp) %>% filter(lake == "Paul") %>% select(t, temp)
 
 start = "2008-06-01"
 end = "2011-08-15"
@@ -328,4 +352,18 @@ heatwaves = detect_event(climOutput)
 event_line(heatwaves, metric = "intensity_max", start_date = start, end_date = end)+
   geom_point()
 
+
+##### 2013-2015 Calculating heatwaves #####
+hwTest2 <- sonde13_15mean %>% rename(t = date, temp = mean_temp) %>% filter(lake == "L") %>% select(t, temp)
+
+
+hwtest3 <- rbind(hwTest1, hwTest2)
+start = "2008-06-01"
+end = "2015-09-04"
+
+climOutput = ts2clm(hwtest3, climatologyPeriod = c(start, end))
+heatwaves = detect_event(climOutput)
+
+event_line(heatwaves, metric = "intensity_max", start_date = "2013-06-01", end_date = "2013-09-15")+
+  geom_point()
 
